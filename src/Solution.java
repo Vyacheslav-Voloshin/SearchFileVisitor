3244de069584308a3c5fbe9aeea97623327e2e03
Vyacheslav-Voloshin <oks.volosh1na1981@gmail.com>
2024-08-08 21:55:04 +0300

    first commit

---
+++ src/Solution.java
@@ -0,0 +1,23 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        SearchFileVisitor searchFileVisitor = new SearchFileVisitor();
+
+        searchFileVisitor.setPartOfName("x");
+        searchFileVisitor.setPartOfContent("Slon");
+        searchFileVisitor.setMinSize(1);
+        searchFileVisitor.setMaxSize(10000000);
+
+        Files.walkFileTree(Paths.get("C:/Users/Админ/Desktop"), searchFileVisitor);
+
+        List<Path> foundFiles = searchFileVisitor.getFoundFiles();
+        for (Path file : foundFiles) {
+            System.out.println(file);
+        }
+    }
+}
